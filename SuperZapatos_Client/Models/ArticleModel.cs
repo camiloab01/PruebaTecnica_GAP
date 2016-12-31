97c5e77ebf632862b8dbab0dec053c2bb7f75a28
camiloab01 <camiloab01@gmail.com>
2016-12-31 00:50:43 -0600

    Added MVC models

---
+++ SuperZapatos_Client/Models/ArticleModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperZapatos_Client.Models
+{
+    public class ArticleModel
+    {
+        /// <summary>
+        /// Identifier for the article 
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the article
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Description of the article
+        /// </summary>
+        public String Description { get; set; }
+
+        /// <summary>
+        /// Price of the article
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Total of articles left in shelf
+        /// </summary>
+        public int Total_in_shelf { get; set; }
+
+        /// <summary>
+        /// Total of articles left in vault
+        /// </summary>
+        public int Total_in_vault { get; set; }
+
+        /// <summary>
+        /// Store name where the article is
+        /// </summary>
+        public String Store_name { get; set; }
+    }
+}
---
+++ SuperZapatos_Client/Models/StoreModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperZapatos_Client.Models
+{
+    public class StoreModel
+    {
+        /// <summary>
+        /// Identifier for the store entity
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the store
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Address of the store
+        /// </summary>
+        public String Address { get; set; }
+    }
+}
