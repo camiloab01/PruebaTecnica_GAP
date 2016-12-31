7471d39e733ac5c65b4a10d9c7622b3f5c806ba5
camiloab01 <camiloab01@gmail.com>
2016-12-31 01:13:36 -0600

    Added the views in the MVC Client

---
+++ SuperZapatos_Client/Controllers/ArticlesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SuperZapatos_Client.Models;
+
+namespace SuperZapatos_Client.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private SuperZapatos_ClientContext db = new SuperZapatos_ClientContext();
+
+        // GET: Articles
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.ArticleModels.ToListAsync());
+        }
+
+        // GET: Articles/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ArticleModel articleModel = await db.ArticleModels.FindAsync(id);
+            if (articleModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articleModel);
+        }
+
+        // GET: Articles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Description,Price,Total_in_shelf,Total_in_vault,Store_name")] ArticleModel articleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ArticleModels.Add(articleModel);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(articleModel);
+        }
+
+        // GET: Articles/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ArticleModel articleModel = await db.ArticleModels.FindAsync(id);
+            if (articleModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articleModel);
+        }
+
+        // POST: Articles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Description,Price,Total_in_shelf,Total_in_vault,Store_name")] ArticleModel articleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(articleModel).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(articleModel);
+        }
+
+        // GET: Articles/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ArticleModel articleModel = await db.ArticleModels.FindAsync(id);
+            if (articleModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articleModel);
+        }
+
+        // POST: Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ArticleModel articleModel = await db.ArticleModels.FindAsync(id);
+            db.ArticleModels.Remove(articleModel);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
